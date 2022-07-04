ced8200b2dc6fd3a675d8851aee7684cb428e01e
Imre 'Rover' Racz <racz.imre.levente@gmail.com>
2022-07-04 14:14:56 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PaylikeClient
+
+A description of this package.
---
+++ Tests/PaylikeClientTests/PaylikeClientTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import PaylikeClient
+
+final class PaylikeClientTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(PaylikeClient().text, "Hello, World!")
+    }
+}
