da3ad7cc2a3001790d20b298d1e1c1223a305508
Laszlo Kocsis <kocsislaci95@gmail.com>
2023-05-19 13:22:41 +0200

    feat(client): created client protocol to invert dependency on higher packages

--- Sources/PaylikeClient/PaylikeClient.swift
+++ Sources/PaylikeClient/PaylikeClient.swift
@@ -1,9 +1,61 @@
 import PaylikeRequest
 
+
+/**
+ * Describing the necessary function for the client
+ */
+public protocol Client {
+    /**
+     * Client ID sent to the API to identify the client connection interface
+     */
+    var clientID: String { get set }
+    /**
+     * Underlying httpClient implementation used
+     */
+    var httpClient: HTTPClient { get set }
+    /**
+     * Used for logging, called when the request is constructed
+     */
+    var loggingFn: (Encodable) -> Void { get set }
+    /**
+     * Calling the tokenization API
+     */
+    func tokenize(
+        applePayData data: TokenizeApplePayDataRequest,
+        withCompletion handler: @escaping (Result<ApplePayToken, Error>) -> Void
+    ) -> Void
+    /**
+     * Calling the tokenization API
+     */
+    func tokenize(
+        cardData data: TokenizeCardDataRequest,
+        withCompletion handler: @escaping (Result<CardDataToken, Error>) -> Void
+    ) -> Void
+    /**
+     * Calling the tokenization API
+     */
+    func tokenize(applePayData data: TokenizeApplePayDataRequest) async throws -> ApplePayToken
+    /**
+     * Calling the tokenization API
+     */
+    func tokenize(cardData data: TokenizeCardDataRequest) async throws -> CardDataToken
+    /**
+     * Used for creating and executing the payment flow
+     */
+    func createPayment(
+        with requestData: CreatePaymentRequest,
+        withCompletion handler: @escaping (Result<PaylikeClientResponse, Error>) -> Void
+    ) -> Void
+    /**
+     * Same functionality as `createPayment(..., withCompletion:)` but with async await syntax features
+     */
+    func createPayment(with requestData: CreatePaymentRequest) async throws -> PaylikeClientResponse
+}
+
 /**
  * Handles high level requests toward the Paylike APIs
  */
-public final class PaylikeClient {
+public final class PaylikeClient: Client {
     /**
      * Client ID sent to the API to identify the client connection interface
      */
